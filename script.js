dda15eafb007782bc0c49ee083afe7cbc1bdc2a7
yashhhh2209 <vishuswami683@gmail.com>
2025-02-09 14:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('login-form');
+const codeInput = document.getElementById('code');
+const submitBtn = document.getElementById('submit-btn');
+const resultDiv = document.getElementById('result');
+
+const secretCode = 'shreya'; // Replace with your secret code
+const nextPageUrl = 'main.html'; // Replace with the URL of the new page
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const userInput = codeInput.value.trim();
+	if (userInput === secretCode) {
+		resultDiv.innerHTML = 'okkk';
+		window.location.href = nextPageUrl; // Redirect to new page
+	} else {
+		resultDiv.innerHTML = 'tum kon h??';
+	}
+});
